8ba5a7e6ee85762f4a8d7389051051f42f3deb2d
Midhat-dot <midhatcodes@gmail.com>
2025-07-23 13:36:58 -0700

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold">Daily Notes App</h1>
+      <h1 className="text-3xl font-bold">Daily Notes App!!</h1>
     </div>
   )
 }
