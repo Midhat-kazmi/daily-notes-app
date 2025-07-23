51f7fb83bbabe8a9fdb59211f4be550d90fc4c67
Midhat-dot <midhatcodes@gmail.com>
2025-07-23 13:29:06 -0700

    Initial commit: frontend and backend folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">Daily Notes App</h1>
+    </div>
+  )
+}
+
+export default App
